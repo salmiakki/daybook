e3752f7daac3a0667db4b13f3b4bd08708fb8817
Lesha Pak <leshapak@yandex-team.ru>
2021-05-05 18:05:57 +0300

    Refactor

--- daybook.py
+++ daybook.py
@@ -11,19 +11,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PROGRESS_21_PATH = os.getenv("DAYBOOK_DEFAULT_PATH")
-SHOW_HOW_MANY_LAST_LINES = 10
+DEFAULT_PATH = os.getenv("DAYBOOK_DEFAULT_PATH")
+SHOW_HOW_MANY_LAST_LINES = int(os.getenv("DAYBOOK_SHOW_HOW_MANY_LAST_LINES", 10))
 
 
-def add_line(old_content: str, line: str, trim_end=True) -> str:
+def add_line(old_content: str, line: str, trim_end=True, allow_duplicates=False) -> str:
     lines = old_content.splitlines()
-    if line in old_content:
-        old_line_number_with_new_line = None
+    if not allow_duplicates:
         for i, old_line in enumerate(lines, 1):
             if line in old_line:
-                old_line_number_with_new_line = i
-                break
-        raise ValueError(f"{line} is already there (line {old_line_number_with_new_line}).")  #
+                old_line_with_new_line_number = i
+                raise ValueError(f"{line} is already there (line {old_line_with_new_line_number}).")
 
     formatted_line = format_line(line)
     lines.append(formatted_line)
@@ -55,13 +53,15 @@ def add_dateline(old_content, date: Optional[datetime.date] = None) -> str:
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-p", "--path", type=Path, help="path to consume", default=Path(PROGRESS_21_PATH))
-    arg_parser.add_argument("line")
+    arg_parser.add_argument("-p", "--path", type=Path, help="path to the daybook file", default=Path(DEFAULT_PATH))
+    # arg_parser.add_argument("-v", "--verbose", type=Path, action="store_true")
+    arg_parser.add_argument("line", help="the line to add")
     args = arg_parser.parse_args()
 
     line = args.line
     path: Path = args.path
     if path:
+        logger.info(f"Adding to {path}")
         old_content = path.read_text()
         new_content = add_dateline(old_content)
         new_content = add_line(new_content, line)
