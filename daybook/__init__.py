a3c467a3705278701e04c87b49669332339d1482
Lesha Pak <leshapak@yandex-team.ru>
2021-05-05 21:33:20 +0300

    Add setup.py

---
+++ daybook/__init__.py
@@ -0,0 +1,1 @@
+from .daybook import main
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="daybook",
+    version="0.1",
+    description="URL hoarder's helper utility",
+    url="https://github.com/salmiakki/daybook",
+    author="Lesha Pak",
+    author_email="kapahel@gmail.com",
+    packages=["daybook"],
+    zip_safe=False,
+    install_requires=[
+        "loguru>=0.5.3",
+        "python-dotenv>=0.17.1",
+    ],
+    entry_points={
+        "console_scripts": ["daybook=daybook.daybook:main"],
+    },
+)
